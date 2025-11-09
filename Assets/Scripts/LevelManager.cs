eb35661e837ed9136a0d46e95762ec84481ca150
Biscuitbunny <60516813+Biscuitbunny@users.noreply.github.com>
2025-11-09 14:58:03 +0100

    Some UI Adjustments etc

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -8,21 +8,47 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private GameObject gameGridObject;
     [SerializeField] private GameObject lvlCompleteScreen;
 
+    [SerializeField] private float waitTime = 3f;
+
+    [SerializeField] private AudioClip successSound;
+    [SerializeField] private float successVolume = 1;
+
+    
     public void CompleteLevel()
     {
+        StartCoroutine(ChangeScreen());
+
+        //Play SoundFX & VFX
+        SoundFXManager.instance.PlaySoundFXClip(successSound, transform, successVolume);
+    }
+
+    IEnumerator ChangeScreen()
+    {
+        //Wait until SoundFX and VFX are complete
+        yield return new WaitForSeconds(waitTime);
+
+        //Disable game-relevant gameobjects
         activeGameScreen.SetActive(false);
         gameGridObject.SetActive(false);
 
-        foreach (Tile tile in Resources.FindObjectsOfTypeAll(typeof(Tile)) as Tile[])
+        foreach (Tile tile in FindObjectsOfType<Tile>())
         {
             tile.gameObject.SetActive(false);
         }
 
-        foreach (Item item in Resources.FindObjectsOfTypeAll(typeof(Item)) as Item[])
+        foreach (Item item in FindObjectsOfType<Item>())
         {
             item.gameObject.SetActive(false);
         }
 
+        GameObject[] stringList = GameObject.FindGameObjectsWithTag("String");
+
+        foreach (GameObject stringGO in stringList)
+        {
+            stringGO.SetActive(false);
+        }
+
+        //Enable level complete screen
         lvlCompleteScreen.SetActive(true);
     }
 }
