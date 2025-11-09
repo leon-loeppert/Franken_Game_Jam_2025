9ae4487c32427ad18664518a94f44aea959a167e
Biscuitbunny <60516813+Biscuitbunny@users.noreply.github.com>
2025-11-09 15:46:54 +0100

    Some Sound Implementation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,14 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public float levelLoadWait = 0.5f;
     public void LoadLevel(string levelName)
     {
-        SceneManager.LoadScene(levelName);
+        StartCoroutine(LoadSceneAfterWait(levelName));
+        
     }
 
     public void QuitGame()
     {
         print("Quit Game");
         Application.Quit();
+    }
+
+    IEnumerator LoadSceneAfterWait(string levelName)
+    {
+        yield return new WaitForSeconds(levelLoadWait);
+        SceneManager.LoadScene(levelName);
     }
 }
